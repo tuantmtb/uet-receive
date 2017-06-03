87ca19a95c8d21be0327befbbe0e345f79de44bc
Tran Minh Tuan <tuantmtb@gmail.com>
2017-06-03 21:31:09 +0700

    update

--- config/fb-messenger.php
+++ config/fb-messenger.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'debug' => env('APP_DEBUG', false),
-    'verify_token' => env('MESSENGER_VERIFY_TOKEN'),
-    'app_token' => env('MESSENGER_APP_TOKEN'),
+    'verify_token' => 'UETer_bot',
+    'app_token' => 'EAABZCTdZCzmv4BADoZB56rhJ8BDeZAPgOEXkUGsuE1lNg2sH3o6ZC6HbU3uNIdVSp5udAsRRoxstNwar9lse8M15xdjceQLWZCZCesgdIj4gVKHtdzZAsmffxOe4ov4mQZB8R7tCNtbDh6YGrpuqKWUSRPmM5JRH3ciaBZCVQTGSPAugZDZD',
     'app_secret' => env('MESSENGER_APP_SECRET', null),
     'auto_typing' => true,
     'handlers' => [
@@ -11,3 +11,15 @@ return [
     'custom_url' => '/webhook',
     'postbacks' => [],
 ];
+//return [
+//    'debug' => env('APP_DEBUG', false),
+//    'verify_token' => env('MESSENGER_VERIFY_TOKEN'),
+//    'app_token' => env('MESSENGER_APP_TOKEN'),
+//    'app_secret' => env('MESSENGER_APP_SECRET', null),
+//    'auto_typing' => true,
+//    'handlers' => [
+//        Casperlaitw\LaravelFbMessenger\Contracts\DefaultHandler::class
+//    ],
+//    'custom_url' => '/webhook',
+//    'postbacks' => [],
+//];
