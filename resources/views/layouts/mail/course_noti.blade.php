e24cc6e320072d014a86a253dec487dc3dcda1a7
Tran Minh Tuan <tuantmtb@gmail.com>
2017-06-05 11:44:27 +0700

    update downloader

--- resources/views/layouts/mail/course_noti.blade.php
+++ resources/views/layouts/mail/course_noti.blade.php
@@ -3,7 +3,8 @@
 <ul>
     <li>Tên môn học: {{$course->name}}</li>
     <li>Mã môn học: {{$course->code}}</li>
-    <li>Tải về danh sách điểm {{Html::link($course->link_origin, 'tại đây')}}</li>
+    <li>Tải về danh sách điểm {{Html::link(config('app.env') . 'link=?'.$course->link_remote, 'tại đây')}}</li>
+    <li>Hoặc {{Html::link($course->link_origin, 'tại đây')}}</li>
     <li>Xem các môn khác <a href="http://ueter.xyz">tại đây</a></li>
 </ul>
 <div>Best regards,</div>
